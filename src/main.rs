302d70ed185842131eb6c85dcc5949aa5d3a5108
lvbin <lvbin@hnnewsoft.com>
2021-09-22 13:56:05 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -27,12 +27,13 @@ use futures::stream::TryStreamExt;
 use time;
 
 
-
 #[tokio::main]
 async fn main() {
+    println!("==========");
     if_multipart_then_upload_multiparts_dicom().await;
     println!("==========");
-    // get_object().await;
+    get_object().await;
+    // upload().await;
 }
 
 #[tokio::test]
@@ -48,41 +49,55 @@ async fn calc_md5() {
     }
 }
 
-
 async fn get_object() {
-    let destination_filename = "test_witness_1";
-    let bucket_name = "heco-manager-s3-test";
-    let client = S3Client::new(Region::CnNorth1);
+    let now = Instant::now();
+    let destination_filename = "test_witness_2";
+    let bucket_name = "zkdex-prod-xingchen-files";
+    // let bucket_name = "heco-manager-s3-test";
+    // let client = S3Client::new(Region::ApNortheast1);
+    let client = S3Client::new(Region::ApNortheast1);
     let get_object_request = GetObjectRequest {
         bucket: bucket_name.to_owned(),
         key: destination_filename.to_owned(),
         ..Default::default()
     };
-    let time = time::get_time();
-    println!("1: {}",format!("{:#?}",time.sec));
-
-    let  mut object = client.get_object(get_object_request).await.expect("get object failed");
-    let time = time::get_time();
-    println!("2: {}",format!("{:#?}",time.sec));
+    let mut object = client.get_object(get_object_request).await.expect("get object failed");
     let body = object.body.take().expect("The object has no body");
 
-    //to string
+    // to string
     let body = body.map_ok(|b| b.to_vec()).try_concat().await.expect("ff");
-    let time = time::get_time();
-    println!("3: {}",format!("{:#?}",time.sec));
-    let res_str = std::str::from_utf8(&body).expect("fe");
+    let res_str = std::str::from_utf8(&body).expect("fail to str");
     let mut md5 = Md5::new();
     md5.input_str(res_str);
     println!("md5:{}", md5.result_str());
-    println!("body length: {}", res_str.len());
-
 
     // write to file
-    let mut body_read = body.into_async_read();
-    let mut file = tokio::fs::File::create("./witness_get_new").await.expect("fc");
-    tokio::io::copy(&mut body_read, &mut file).await;
-    println!("3: {}",format!("{:#?}",time.sec));
-    // println!("ed {}", a.len());
+    // let mut body_read = body.into_async_read();
+    // let mut file = tokio::fs::File::create("./ww_new").await.expect("fc");
+    // tokio::io::copy(&mut body_read, &mut file).await;
+
+    println!("task taken : {}", now.elapsed().as_secs());
+}
+
+async fn upload() {
+    let local_filename = "/Users/lvbin/Desktop/a";
+    let destination_filename = "test_witness_2";
+    // let bucket_name = "heco-manager-s3-test";
+    let bucket_name = "zkdex-prod-xingchen-files";
+    let destination_filename_clone = destination_filename.clone();
+    let mut file = std::fs::File::open(local_filename).unwrap();
+    let mut buffer = String::new();
+    let res = file.read_to_string(&mut buffer);
+    let body = buffer.into_bytes();
+    let client = S3Client::new(Region::ApNortheast1);
+    let por = PutObjectRequest {
+        body: Some(body.into()),
+        bucket: bucket_name.to_string().to_owned(),
+        key: destination_filename_clone.to_owned(),
+        ..Default::default()
+    };
+    let res = client.put_object(por).await.expect("fail ");
+    println!("{:?}", res);
 }
 
 async fn if_multipart_then_upload_multiparts_dicom() {
@@ -94,11 +109,16 @@ async fn if_multipart_then_upload_multiparts_dicom() {
     let bucket_name = "zkdex-prod-xingchen-files";
     let destination_filename_clone = destination_filename.clone();
     let mut file = std::fs::File::open(local_filename).unwrap();
-    const CHUNK_SIZE: usize = 6_000_000;
-    let mut buffer = Vec::with_capacity(CHUNK_SIZE);
+    let mut buffer = String::new();
+    let res = file.read_to_string(&mut buffer);
+    let data_send_base = buffer.into_bytes();
 
-    // let client = S3Client::new(Region::CnNorth1);
+
+    const CHUNK_SIZE: usize = 6_000_000;
+    // let mut buffer = Vec::with_capacity(CHUNK_SIZE);
+
     let client = S3Client::new(Region::ApNortheast1);
+    // let client = S3Client::new(Region::ApNortheast1);
     let create_multipart_request = CreateMultipartUploadRequest {
         bucket: bucket_name.to_owned(),
         key: destination_filename.to_owned(),
@@ -131,28 +151,40 @@ async fn if_multipart_then_upload_multiparts_dicom() {
 
     let mut multiple_parts_futures = Vec::new();
     loop {
-        let maximum_bytes_to_read = CHUNK_SIZE - buffer.len();
-        println!("maximum_bytes_to_read: {}", maximum_bytes_to_read);
-        file.by_ref()
-            .take(maximum_bytes_to_read as u64)
-            .read_to_end(&mut buffer)
-            .unwrap();
-        println!("length: {}", buffer.len());
-        println!("part_number: {}", part_number);
-        if buffer.len() == 0 {
-            // The file has ended.
+        // let maximum_bytes_to_read = CHUNK_SIZE - buffer.len();
+        // println!("maximum_bytes_to_read: {}", maximum_bytes_to_read);
+        // file.by_ref()
+        //     .take(maximum_bytes_to_read as u64)
+        //     .read_to_end(&mut buffer)
+        //     .unwrap();
+        // println!("length: {}", buffer.len());
+        // println!("part_number: {}", part_number);
+        // if buffer.len() == 0 {
+        //     println!("the file is end");
+        //     // The file has ended.
+        //     break;
+        // }
+
+        if (part_number - 1) * CHUNK_SIZE > data_send_base.len() {
+            println!("the file is end");
             break;
         }
-        let next_buffer = Vec::with_capacity(CHUNK_SIZE);
-        let data_to_send = buffer;
+        let start = (part_number - 1) * CHUNK_SIZE;
+        let mut end = part_number * CHUNK_SIZE;
+        if end > data_send_base.len(){
+            end = data_send_base.len();
+        }
+        // let next_buffer = Vec::with_capacity(CHUNK_SIZE);
+        let data_to_send = Vec::from(&data_send_base[start..end]);
+        println!("part_number: {}: len:{} ", part_number, data_to_send.len());
         let completed_parts_cloned = completed_parts.clone();
         let create_upload_part_arc_cloned = create_upload_part_arc.clone();
         let send_part_task_future = tokio::task::spawn(async move {
-            let part = create_upload_part_arc_cloned(data_to_send.to_vec(), part_number);
+            let part = create_upload_part_arc_cloned(data_to_send, part_number as i64);
             {
                 let part_number = part.part_number;
                 // let client = super::get_client().await;
-                let client = S3Client::new(Region::CnNorth1);
+                let client = S3Client::new(Region::ApNortheast1);
                 let response = client.upload_part(part).await;
                 completed_parts_cloned.lock().unwrap().push(CompletedPart {
                     e_tag: response
@@ -164,11 +196,11 @@ async fn if_multipart_then_upload_multiparts_dicom() {
             }
         });
         multiple_parts_futures.push(send_part_task_future);
-        buffer = next_buffer;
+        // buffer = next_buffer;
         part_number = part_number + 1;
     }
     // let client = super::get_client().await;
-    let client = S3Client::new(Region::CnNorth1);
+    let client = S3Client::new(Region::ApNortheast1);
     println!("waiting for futures");
     let _results = futures::future::join_all(multiple_parts_futures).await;
 
